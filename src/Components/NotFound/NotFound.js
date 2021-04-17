91916506d7e95a950b3a208b7cc9fc74be3c3f0c
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-04-17 22:59:42 +0600

    update

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,15 +4,9 @@ const NotFound = () => {
     return (
         <div style={{textAlign: 'center'}}>
             <h1>404, We Can't get Searching result</h1>
-            <P>echo "# complete-website-client-nurulabsar-git" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Porgramming-Hero-web-course/complete-website-client-nurulabsar-git.git
-git push -u origin main</P>
+            
         </div>
-    );
+    )
 };
 
 export default NotFound;
